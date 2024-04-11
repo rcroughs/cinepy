2ac7739065031733148b4352039ab010b0e8b39d
Romain Croughs <63919848+rcroughs@users.noreply.github.com>
2024-04-11 17:14:59 +0200

    Better displaying of the showtimes (#5)

--- cine.py
+++ cine.py
@@ -3,28 +3,48 @@
 from cine.api.letterboxd.WatchList import WatchList
 from cine.api.Showtimes import Showtimes
 from datetime import datetime, timedelta
+from cine.api.Show import Show
+from math import log
 import cine.colors as colors
 import argparse
 
-def display_showtimes_with_wathlist(showtimes, watchlist):
+
+def println(found_in_watchlist: bool, counter: int, showtime: Show, title_width: int, theater_width: int, date_width: int, length):
+    line = "⭐" if found_in_watchlist else "  "
+    counter_string = f"({counter:0{len(str(length))}d}) "
+    title = (colors.YELLOW if found_in_watchlist else "") + showtime.getTitle().ljust(title_width) + colors.RESET
+    theater = showtime.getTheater().ljust(theater_width)
+    date = str(showtime.getStartDate()).ljust(date_width)
+    not_found = f"{colors.DARK_GRAY}?{colors.RESET}" if showtime.getTMDBID() is None else ""
+    print(line + counter_string + title + theater + date + not_found)
+
+def getWidths(showtimes):
+    max_title_width = 0
+    max_theater_width = 0
+    max_date_width = 0
+    for showtime in showtimes.getShowtimes():
+        max_title_width = max(max_title_width, len(showtime.getTitle()))
+        max_theater_width = max(max_theater_width, len(showtime.getTheater()))
+        max_date_width = max(max_date_width, len(str(showtime.getStartDate())))
+    return max_title_width, max_theater_width, max_date_width
+
+def display_showtimes_with_watchlist(showtimes, watchlist):
     counter = 1
+    max_title_width, max_theater_width, max_date_width = getWidths(showtimes)
     for showtime in showtimes.getShowtimes():
         found_in_watchlist = False
         for movie in watchlist.getWatchList():
             if movie.getTMDBID() is not None and showtime.getTMDBID() is not None:
                 if int(showtime.getTMDBID()) == int(movie.getTMDBID()):
                     found_in_watchlist = True
-        if found_in_watchlist:
-            print(f"⭐({counter}){colors.YELLOW} {showtime.getTitle()} {colors.RESET} at {showtime.getTheater()} on {showtime.getStartDate()}")
-        else:
-            not_found = f"{colors.DARK_GRAY}?{colors.RESET}" if showtime.getTMDBID() is None else ""
-            print(f"  ({counter}) {showtime.getTitle()} at {showtime.getTheater()} on {showtime.getStartDate()} {not_found}")
+        println(found_in_watchlist, counter, showtime, max_title_width + 7, max_theater_width + 7, max_date_width, len(showtimes.getShowtimes()))
         counter += 1
 
 def display_showtimes(showtimes):
     counter = 1
+    max_title_width, max_theater_width, max_date_width = getWidths(showtimes)
     for showtime in showtimes.getShowtimes():
-        print(f"({counter}) {showtime.getTitle()} at {showtime.getTheater()} on {showtime.getStartDate()}")
+        println(False, counter, showtime, max_title_width + 7, max_theater_width + 7, max_date_width, showtimes.getShowtimes())
         counter += 1
 
 def main(args) -> int:
@@ -37,7 +57,7 @@ def main(args) -> int:
     showtimes = Showtimes(args.cities, dateBegin=current_date, dateEnd=next_day, limit=args.limit)
     if args.account is not None:
         watchlist = WatchList(args.account)
-        display_showtimes_with_wathlist(showtimes, watchlist)
+        display_showtimes_with_watchlist(showtimes, watchlist)
     else:
         display_showtimes(showtimes)
     return 0
