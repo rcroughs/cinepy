8b265e5090fb9db61e3061bae2f527189df6bd4a
rcroughs <romain.croughs@gmail.com>
2024-04-11 15:21:02 +0200

    feat: argparse

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Cinepy
+`cine.py` is a python script that connects the [Cineville](https://cinevillepass.be/) API to different services such as Letterboxd, Google Calendar, etc.
 
+## Usage
+```
+usage: cine.py [-h] [-a ACCOUNT] [-d DAYS] [-c CITIES [CITIES ...]]
+
+Display showtimes of Cinevillepass
+
+options:
+  -h, --help            show this help message and exit
+  -a ACCOUNT, --account ACCOUNT
+                        Letterboxd account name
+  -d DAYS, --days DAYS  Days offset for the query
+  -c CITIES [CITIES ...], --cities CITIES [CITIES ...]
+                        Cities to query
+```
+
+### Dependencies
+All the dependencies are shown in the file `requirement.txt`.
+
+### Greetings
+Some of the code inside this project is based on other projects:
+- [janw/letterboxd-rss](https://github.com/janw/letterboxd-rss)
--- cine.py
+++ cine.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 import cine.colors as colors
 import argparse
 
-def display_showtimes(showtimes, watchlist):
-    counter = 0
+def display_showtimes_with_wathlist(showtimes, watchlist):
+    counter = 1
     for showtime in showtimes.getShowtimes():
         found_in_watchlist = False
         for movie in watchlist.getWatchList():
@@ -21,19 +21,32 @@ def display_showtimes(showtimes, watchlist):
             print(f"  ({counter}) {showtime.getTitle()} at {showtime.getTheater()} on {showtime.getStartDate()} {not_found}")
         counter += 1
 
-def main(*args) -> int:
-    parser = argparse.ArgumentParser(prog="cinepy", description="Display showtimes of Cinevillepass")
-    parser.add_argument("-a", "--account", help="Letterboxd account name", required=False)
-    parser.add_argument("-d", "--days", default=0, help="Days offset for the query", required=False)
+def display_showtimes(showtimes):
+    counter = 1
+    for showtime in showtimes.getShowtimes():
+        print(f"({counter}) {showtime.getTitle()} at {showtime.getTheater()} on {showtime.getStartDate()}")
+        counter += 1
 
-    current_date = datetime.now()
+def main(args) -> int:
+    current_date = datetime.now() + timedelta(days=args.days)
+    # if no days are specified, we want to display the showtimes for the current day
+    if args.days is None:
+        current_day = current_day.replace(hour=0, minute=0, second=0, microsecond=0)
     next_day = current_date + timedelta(days=1)
     next_day = next_day.replace(hour=0, minute=0, second=0, microsecond=0)
-    showtimes = Showtimes(["brussels"], dateBegin=current_date, dateEnd=next_day, limit=50)
-    watchlist = WatchList("korzie")
-    display_showtimes(showtimes, watchlist)
+    showtimes = Showtimes(args.cities, dateBegin=current_date, dateEnd=next_day, limit=50)
+    if args.account is not None:
+        watchlist = WatchList(args.account)
+        display_showtimes_with_wathlist(showtimes, watchlist)
+    else:
+        display_showtimes(showtimes)
     return 0
 
 if __name__ == "__main__":
     import sys
-    sys.exit(main(*sys.argv[1:]))
+    parser = argparse.ArgumentParser(prog="cine.py", description="Display showtimes of Cinevillepass")
+    parser.add_argument("-a", "--account", help="Letterboxd account name", required=False)
+    parser.add_argument("-d", "--days", help="Days offset for the query", type=int, default=0, required=False)
+    parser.add_argument('-c', '--cities', nargs='+', help="Cities to query", default=[], required=False)
+    args = parser.parse_args()
+    sys.exit(main(args))
