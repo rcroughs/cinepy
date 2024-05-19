9713013e36baabf92579ac813f134bba8ccbecbc
rcroughs <romain.croughs@gmail.com>
2024-05-19 18:16:54 +0200

    feat: Add support for ignoring movies already seen in showtimes
    The code changes include:
    - Added the `-i` or `--ignore` option to the `cine.py` script to ignore movies that the user has already seen in the showtimes.
    - Updated the `Theaters` class to accept a `minimal` parameter to control whether to print the "Fetching theaters" message.
    - Updated the `Showtimes` class to accept a `minimal` parameter to control whether to print the "Fetching showtimes" message.
    - Updated the `WatchList` class to accept a `minimal` parameter to control whether to display the progress bar when fetching the watchlist.feat: Add support for ignoring movies already seen in showtimes

--- cine.py
+++ cine.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 from cine.api.letterboxd.WatchList import WatchList
+from cine.api.letterboxd.SeenMovies import SeenMovies
 from cine.api.Showtimes import Showtimes
 from datetime import datetime, timedelta
 from cine.api.Show import Show
 from cine.api.Theaters import Theaters
 from math import log
 import argparse
-from cine.display import display_showtimes, display_showtimes_with_watchlist
+from cine.display import display_showtimes, display_showtimes_with_account
 
 
 
@@ -19,17 +20,18 @@ def main(args) -> int:
     next_day = current_date + timedelta(days=args.period)
     next_day = next_day.replace(hour=0, minute=0, second=0, microsecond=0)
     f_theaters = []
-    theaters = Theaters(args.cities)
+    theaters = Theaters(args.cities, minimal=args.minimal)
     if args.theater is not None:
         for theater in theaters.getTheaters():
             if theater.getSlug() in args.theater:
                 f_theaters.append(theater)
-    showtimes = Showtimes(args.cities, dateBegin=current_date, dateEnd=next_day, theaters=f_theaters)
+    showtimes = Showtimes(args.cities, dateBegin=current_date, dateEnd=next_day, theaters=f_theaters, minimal=args.minimal)
     if args.account is not None:
-        watchlist = WatchList(args.account)
-        display_showtimes_with_watchlist(showtimes, watchlist, theaters)
+        watchlist = WatchList(args.account, minimal=args.minimal)
+        seen_movies = SeenMovies(args.account, minimal=args.minimal)
+        display_showtimes_with_account(showtimes, watchlist, theaters, seen_movies, args.limit, args.ignore, args.minimal)
     else:
-        display_showtimes(showtimes, theaters)
+        display_showtimes(showtimes, theaters, args.limit, args.minimal)
     return 0
 
 if __name__ == "__main__":
@@ -41,5 +43,10 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--cities', nargs='+', help="Cities to query", default=[], required=False)
     parser.add_argument("-l", "--limit", help="Limit of showtimes to fetch", type=int, required=False)
     parser.add_argument("-t", "--theater", help="Theater to query", nargs='+', required=False)
+    parser.add_argument("-i", "--ignore", help="Ignore the movies that you have already seen", action="store_true", required=False)
+    parser.add_argument("-f", "--full", help="Display full showtimes", action="store_true", required=False)
+    parser.add_argument("-m", "--minimal", help="Minimal Display (for fuzzy finding for example)", action="store_true", required=False)
     args = parser.parse_args()
+    if args.full:
+        args.period = 8
     sys.exit(main(args))
--- cine/api/Theaters.py
+++ cine/api/Theaters.py
@@ -3,13 +3,14 @@ from .k_cities import k_cities
 from .Theater import Theater
 
 class Theaters:
-    def __init__(self, cities: list[str]) -> None:
+    def __init__(self, cities: list[str], minimal=False) -> None:
         self._url = "https://cinevillepass.be/api/graphql"
         self._cities = cities
+        self._minimal = minimal
         self._theaters = self.fetchTheaters()
 
     def fetchTheaters(self) -> list[Theater]:
-        print(f"🚀 Fetching theaters")
+        print(f"🚀 Fetching theaters") if not self._minimal else None
         r = requests.post(url = self._url, json = {"operationName": "theaters", "query": "query theaters($filters: TheatersFilters, $collections: [CollectionFilter!], $page: CursorPagination, $sort: TheatersSort, $locale: String) {  theaters(    filters: $filters    collections: $collections    page: $page    sort: $sort    locale: $locale  ) {    data {      ...theater      __typename    }    ...pageInfo    __typename  }}fragment address on Address {  street  houseNumber  postalCode  city  country  __typename}fragment asset on Asset {  url  mime  alternativeText  __typename}fragment theater on Theater {  id  slug  name  address {    ...address    __typename  }  cover {    ...asset    __typename  }  website  shortDescription  intro  description  ticketInfo  __typename}fragment pageInfo on Response {  count  totalCount  previous  next  __typename}", 
                                                     "variables" : {
                                                         "collections": [{"collectionGroupId": "cities", "collectionId": city, "id": str(k_cities[city])} for city in self._cities]
